e9b8e33ddc7b4aedc9aa7d79e361742688d20ab0
Andrew <keenanandrew@gmail.com>
2024-01-26 16:55:01 +0000

    Linting scripts

--- ingest_data_green.py
+++ ingest_data_green.py
@@ -1,8 +1,9 @@
-import os 
+import os
 import argparse
 from time import time
 import pandas as pd
 from sqlalchemy import create_engine
+
 
 def main(params):
     user = params.user
@@ -13,12 +14,10 @@ def main(params):
     table_name = params.table_name
     url = params.url
 
-    if url.endswith('.csv.gz'):
-        csv_name = 'green_output.csv.gz'
+    if url.endswith(".csv.gz"):
+        csv_name = "green_output.csv.gz"
     else:
-        csv_name = 'green_output.csv'
-
-
+        csv_name = "green_output.csv"
 
     os.system(f"wget {url} -O {csv_name}")
 
@@ -26,7 +25,6 @@ def main(params):
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     while True:
-
         try:
             t_start = time()
 
@@ -40,22 +38,22 @@ def main(params):
             t_end = time()
 
             print("inserted another chunk, took %.3f second" % (t_end - t_start))
-        
+
         except StopIteration:
             print("Finishing with all the ingesting.")
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ingesting data from csv to Postgres")
 
-    parser.add_argument('--user', help='username for Postgres')
-    parser.add_argument('--password', help='password for Postgres')
-    parser.add_argument('--host', help='host for Postgres')
-    parser.add_argument('--port', help='port for Postgres')
-    parser.add_argument('--db', help='database name for Postgres')
-    parser.add_argument('--table_name', help='table where results will be written to')
-    parser.add_argument('--url', help='url of CSV file')
+    parser.add_argument("--user", help="username for Postgres")
+    parser.add_argument("--password", help="password for Postgres")
+    parser.add_argument("--host", help="host for Postgres")
+    parser.add_argument("--port", help="port for Postgres")
+    parser.add_argument("--db", help="database name for Postgres")
+    parser.add_argument("--table_name", help="table where results will be written to")
+    parser.add_argument("--url", help="url of CSV file")
 
     args = parser.parse_args()
 
--- ingest_data_yellow.py
+++ ingest_data_yellow.py
@@ -1,8 +1,9 @@
-import os 
+import os
 import argparse
 from time import time
 import pandas as pd
 from sqlalchemy import create_engine
+
 
 def main(params):
     user = params.user
@@ -12,12 +13,12 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    zones_url = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv'
+    zones_url = "https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv"
 
-    if url.endswith('.csv.gz'):
-        csv_name = 'yellow_output.csv.gz'
+    if url.endswith(".csv.gz"):
+        csv_name = "yellow_output.csv.gz"
     else:
-        csv_name = 'yellow_output.csv'
+        csv_name = "yellow_output.csv"
 
     os.system(f"wget {url} -O {csv_name}")
     os.system(f"wget {zones_url} -O {'zones.csv'}")
@@ -27,11 +28,10 @@ def main(params):
     df = next(df_iter)
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
 
-    zones_df = pd.read_csv('zones.csv')
-    zones_df.to_sql(name='zones', con=engine, if_exists='replace')
+    zones_df = pd.read_csv("zones.csv")
+    zones_df.to_sql(name="zones", con=engine, if_exists="replace")
 
     while True:
-
         try:
             t_start = time()
 
@@ -45,22 +45,22 @@ def main(params):
             t_end = time()
 
             print("inserted another chunk, took %.3f second" % (t_end - t_start))
-        
+
         except StopIteration:
             print("Finishing with all the ingesting.")
             break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ingesting data from csv to Postgres")
 
-    parser.add_argument('--user', help='username for Postgres')
-    parser.add_argument('--password', help='password for Postgres')
-    parser.add_argument('--host', help='host for Postgres')
-    parser.add_argument('--port', help='port for Postgres')
-    parser.add_argument('--db', help='database name for Postgres')
-    parser.add_argument('--table_name', help='table where results will be written to')
-    parser.add_argument('--url', help='url of CSV file')
+    parser.add_argument("--user", help="username for Postgres")
+    parser.add_argument("--password", help="password for Postgres")
+    parser.add_argument("--host", help="host for Postgres")
+    parser.add_argument("--port", help="port for Postgres")
+    parser.add_argument("--db", help="database name for Postgres")
+    parser.add_argument("--table_name", help="table where results will be written to")
+    parser.add_argument("--url", help="url of CSV file")
 
     args = parser.parse_args()
 
