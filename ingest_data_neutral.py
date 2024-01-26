b5ae31a669d5f4b655870ec028a3ac7f52bf3d3b
Andrew <keenanandrew@gmail.com>
2024-01-26 17:14:30 +0000

    Neutral option added

---
+++ ingest_data_neutral.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+from time import time
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    # table_name = params.table_name
+    url = params.url
+    colour = params.colour
+
+    if url.endswith(".csv.gz"):
+        csv_name = f"{colour}_output.csv.gz"
+    else:
+        csv_name = f"{colour}_output.csv"
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{db}")
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    while True:
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+            df.to_sql(name=colour, con=engine, if_exists="append")
+
+            t_end = time()
+
+            print("inserted another chunk, took %.3f second" % (t_end - t_start))
+
+        except StopIteration:
+            print("Finishing with all the ingesting.")
+            break
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ingesting data from csv to Postgres")
+
+    parser.add_argument("--user", help="username for Postgres")
+    parser.add_argument("--password", help="password for Postgres")
+    parser.add_argument("--host", help="host for Postgres")
+    parser.add_argument("--port", help="port for Postgres")
+    parser.add_argument("--db", help="database name for Postgres")
+    # parser.add_argument("--table_name", help="table where results will be written to")
+    parser.add_argument("--url", help="url of CSV file")
+    parser.add_argument("--colour", help="the colour of taxi desired")
+
+    args = parser.parse_args()
+
+    main(args)
